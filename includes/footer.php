e78c7137bfc2677f21ff090fcba684df2fa00429
Rosh__ <lucasfabien04@gmail.com>
2022-03-11 10:38:32 +0100

    includes

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<!-- <footer class="footer mt-auto py-3 bg-light fixed-bottom">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer> -->
+
+
+
+
+
+<div>
+  <footer class="border-top pt-3 pb-2">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Accueil</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Concert</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Festival</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Théatre</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Spéctacle</a></li>
+    </ul>
+    <p class="text-center text-muted">&copy;<?= date('Y'); ?> - 24H music - Tous droits réservés</p>
+  </footer>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #25938C;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><img src="images/logo-24h-music - copie.png" alt="" width="35" height="35"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Concert</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Festival</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Théatre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Spéctacle</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Sign up</a></li>
+      </ul>
+
+
+    </div>
+  </div>
+</nav>
+
+  <header class="py-3 mb-4 border-bottom mt-5 pt-4">
+    <div class="container">
+      <form class="ms-3 me-5 searchbar">  
+        <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </div> 
+      </form>
+    </div>
+  </header>
